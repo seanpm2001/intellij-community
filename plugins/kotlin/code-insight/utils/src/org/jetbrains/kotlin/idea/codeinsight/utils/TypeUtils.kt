1d6d23d2310f2714ff69a2652e13f7a8a12b7a44
Jaebaek Seo <jaebaek@google.com>
2022-09-19 20:57:46 +0000

    Add K2 EqualsOrHashCodeInspection

---
+++ plugins/kotlin/code-insight/utils/src/org/jetbrains/kotlin/idea/codeinsight/utils/TypeUtils.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+package org.jetbrains.kotlin.idea.codeinsight.utils
+
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
+import org.jetbrains.kotlin.analysis.api.types.KtType
+
+fun KtAnalysisSession.isNullableAnyType(type: KtType) = type.isAny && type.isMarkedNullable
+
+fun KtAnalysisSession.isNonNullableBooleanType(type: KtType) = type.isBoolean && !type.isMarkedNullable
